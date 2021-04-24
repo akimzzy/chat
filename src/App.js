f9e908f94e35165fff230b21a2c87d0ddfcdb2de
Akimzzy <a.akeem@yahoo.com>
2021-04-24 13:24:07 +0100

    Removed default messaged

--- src/App.js
+++ src/App.js
@@ -10,64 +10,64 @@ function App() {
       type: "bot",
       id: 1,
     },
-    {
-      text:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-      type: "user",
-      id: 2,
-    },
-    {
-      text:
-        "Lorem Ipsum is simply Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-      type: "user",
-      id: 3,
-    },
-    {
-      text: "Okay.",
-      type: "bot",
-      id: 4,
-    },
-    {
-      text:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text",
-      type: "bot",
-      id: 5,
-    },
-    {
-      text:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-      type: "user",
-      id: 6,
-    },
-    {
-      text:
-        "Lorem Ipsum is simply Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-      type: "user",
-      id: 7,
-    },
-    {
-      text: "Okay.",
-      type: "bot",
-      id: 8,
-    },
-    {
-      text:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-      type: "user",
-      id: 9,
-    },
-    {
-      text:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-      type: "user",
-      id: 10,
-    },
-    {
-      text:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-      type: "user",
-      id: 11,
-    },
+    // {
+    //   text:
+    //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    //   type: "user",
+    //   id: 2,
+    // },
+    // {
+    //   text:
+    //     "Lorem Ipsum is simply Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    //   type: "user",
+    //   id: 3,
+    // },
+    // {
+    //   text: "Okay.",
+    //   type: "bot",
+    //   id: 4,
+    // },
+    // {
+    //   text:
+    //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text",
+    //   type: "bot",
+    //   id: 5,
+    // },
+    // {
+    //   text:
+    //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    //   type: "user",
+    //   id: 6,
+    // },
+    // {
+    //   text:
+    //     "Lorem Ipsum is simply Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    //   type: "user",
+    //   id: 7,
+    // },
+    // {
+    //   text: "Okay.",
+    //   type: "bot",
+    //   id: 8,
+    // },
+    // {
+    //   text:
+    //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    //   type: "user",
+    //   id: 9,
+    // },
+    // {
+    //   text:
+    //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    //   type: "user",
+    //   id: 10,
+    // },
+    // {
+    //   text:
+    //     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    //   type: "user",
+    //   id: 11,
+    // },
   ];
   const [allMessages, setAllMessages] = useState(defaultMessages);
 
